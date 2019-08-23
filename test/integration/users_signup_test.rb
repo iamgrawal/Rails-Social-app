8453e30abcbdb7d04d034a66e059490808df3cf0
iamgrawal <gaurav.rawal66@gmail.com>
2019-08-23 15:57:29 +0530

    controoller testing gem added:

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -8,7 +8,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end 
     assert_template 'users/new'
   end
-  test "valis signup information" do
+  test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
       post users_path, params: { user: {name: "Example User", email: "user@example.com", password: "password", password_confirmation: "password"}}
