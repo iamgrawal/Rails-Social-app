978662c862d7e2d809cb5238e4a930ac58343d2e
iamgrawal <gaurav.rawal66@gmail.com>
2019-08-26 08:15:30 +0530

    ch-11-account-activation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -57,6 +57,6 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
   test "authenticated? should return false for user with nil digest" do
-    assert_not @user.authenticated?('')
+    assert_not @user.authenticated?(:remember, '')
   end
 end
